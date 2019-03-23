b4b7019b69b2ef348755651c3b31c9a3f2f89c8c
nexylmao <nenadvvvvvvv@gmail.com>
2019-03-23 21:25:07 +0100

    Making the requests, debugUser

--- modules/router-provider.js
+++ modules/router-provider.js
@@ -2,12 +2,54 @@ const express = require('express');
 
 const filters = require('./router-filters');
 const limiters = require('./router-limiters');
+const env = require('./environment-variables');
 
 module.exports = async function (methods) {
+	/*
+		Initializing the router
+		We'll also create a debugUser for when debug mode is on
+		You can login without any users in the database
+	 */
 	const router = express.Router(); // eslint-disable-line new-cap
 	const logger = await require('./logger-provider')('authenticationRouter');
 
-	router.use((req, res, next) => {
+	let debugUser = null;
+	if (env.debug) {
+		const Chance = require('chance');
+		const chance = new Chance();
+		debugUser = {
+			identification: chance.first({gender: 'female'}),
+			password: chance.last()
+		};
+		logger.debug('debugUser initiated: ');
+		logger.debug(debugUser);
+	}
+
+	router.use(async (req, res, next) => {
+
+		/*
+			We're checking if the user is already logged in.
+			If it is, we need to check if the password is still the same.
+		 */
+		if (req.session.token) {
+			/*
+				Query the database for the user
+				Or check if the user matches the debugUser
+			 */
+			if (req.session.token.identification === debugUser.identification &&
+					req.session.token.password === debugUser.password) {
+				return next();
+			}
+
+			const account = await methods.account.findByIdentification(req.session.token.identification);
+			// No more account -> The account has been deleted
+			// If the password is changed -> Just logging out previous sessions
+			if (!account || (req.session.token.password !== account.password)) {
+				delete req.session.token;
+				return next({code: 401, message: 'You have been logged out!'});
+			}
+		}
+
 		/*
 			We want to log any requests that have finished
 			This is where the object is created
@@ -24,17 +66,40 @@ module.exports = async function (methods) {
 				date: new Date(),
 				statusCode: res.statusCode
 			};
-			if (req.session.token) {
+			if (req.session.token && (req.session.token.identification !== debugUser.identification)) {
 				requestEntry.accountId = req.session.token.identification;
 			}
 			// console.log(requestEntry);
 			methods.requests.create(requestEntry);
 		});
-		next();
+		return next();
 	});
 
 	router.post('/login', limiters.limitLogin, filters.filterLogin, async (req, res, next) => {
 		try {
+			/*
+				If the user is logged in
+			 */
+			if (req.session.token) {
+				return next({code: 401, message: 'You are already logged in!'});
+			}
+
+			/*
+				Debug mode login
+				Made for testing
+			 */
+
+			if (env.debug && (req.body.identification === debugUser.identification && req.body.password === debugUser.password)) {
+				req.session.token = {
+					identification: debugUser.identification,
+					password: debugUser.password
+				};
+				return res.status(200).send({
+					account: debugUser,
+					message: 'Logged in as debug user.'
+				});
+			}
+
 			/*
 				Check provided identification
 				And then, if it exists, calculate the comboHash
@@ -44,6 +109,13 @@ module.exports = async function (methods) {
 			if (!account) {
 				return next({code: 404, message: 'The username/email you entered, does not exist!'});
 			}
+			if (req.body.password !== account.password) {
+				return next({code: 400, message: 'The password you entered, is not correct!'});
+			}
+			req.session.token = {
+				identification: account.id,
+				password: account.password
+			};
 			return res.status(200).send({
 				account
 			});
@@ -52,9 +124,50 @@ module.exports = async function (methods) {
 		}
 	});
 
-	router.get('/test/notImplemented', async (req, res, next) => {
-		return next(new Error('This request is not implemented!'));
+	router.use(async (req, res, next) => {
+		/*
+			This is a gate for logged in users.
+			Past this, users must be logged in.
+			Will check if users info is still relevant
+		 */
+		if (!req.session.token) {
+			return next({code: 403, message: 'You are not logged in!'});
+		}
+
+		return next();
 	});
+
+	/*
+		This requests has no filters, and no data will be used.
+		Testing request used just to test
+		Can be used to see as who you are logged in.
+		(For now, but will be used to provide users data)
+	 */
+	router.get('/me', async (req, res, next) => {
+		return res.status(200).send({
+			account: {
+				identification: req.session.token.identification,
+				password: req.session.token.password
+			}
+		});
+	});
+
+	/*
+		This requests has no filters, and no data will be used.
+		Just to clear the token
+	 */
+	router.post('/logout', async (req, res, next) => {
+		delete req.session.token;
+		return res.status(200).send({
+			message: 'You have successfully logged out!'
+		});
+	});
+
+	/*
+	 	router.get('/test/notImplemented', async (req, res, next) => {
+	  		return next(new Error('This request is not implemented!'));
+	 	});
+	*/
 
 	router.use((error, req, res, _) => {
 		/*
