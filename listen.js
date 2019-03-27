8ee902225a8751d265736a8130d908f0bbe44550
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2019-03-27 14:01:19 +0100

    Testing

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const Redis = require('ioredis');
 
+const eerie = require('./eerie');
 const env = require('./modules/environment-variables');
 
 const port = 8000;
@@ -10,7 +11,25 @@ const routine = async function () {
 		This is how you initialize the library, with some options, debug switch
 		And the sequelize/mongoose connection, and redis connection
 	 */
-	const server = await require('./eerie')({
+	const sequelize = new Sequelize({
+		host: env.sequelize.host,
+		username: env.sequelize.authenticationData.username,
+		password: env.sequelize.authenticationData.password,
+		dialect: env.sequelize.databaseType,
+		database: env.sequelize.initial,
+		dialectOptions: {
+			encrypt: env.sequelize.encrypt
+		}
+	});
+
+	const redis = new Redis({
+		host: env.redis.host,
+		password: env.redis.password,
+		port: env.redis.ssl ? 6380 : 6379,
+		tls: env.redis.ssl
+	});
+
+	const server = await eerie({
 		debug: env.debug,
 		secret: env.secret,
 		options: {
@@ -21,22 +40,8 @@ const routine = async function () {
 			loginAfterRegister: env.options.loginAfterRegister,
 			passwordMethod: env.options.passwordMethod
 		},
-		sequelize: new Sequelize({
-			host: env.sequelize.host,
-			username: env.sequelize.authenticationData.username,
-			password: env.sequelize.authenticationData.password,
-			dialect: env.sequelize.databaseType,
-			database: env.sequelize.initial,
-			dialectOptions: {
-				encrypt: env.sequelize.encrypt
-			}
-		}),
-		redis: new Redis({
-			host: env.redis.host,
-			password: env.redis.password,
-			port: env.redis.ssl ? 6380 : 6379,
-			tls: env.redis.ssl
-		})
+		sequelize,
+		redis
 	});
 
 	const logger = await require('./modules/logger-provider')('serverStarter');
--- test/test.js
+++ test/test.js
@@ -1,15 +1,60 @@
+const Sequelize = require('sequelize');
+const Redis = require('ioredis');
+
+const eerie = require('../eerie');
 const loggerProvider = require('../modules/logger-provider');
+const env = require('../modules/environment-variables');
 
-describe('console.log test', function () {
+const sequelize = new Sequelize({
+	host: env.sequelize.host,
+	username: env.sequelize.authenticationData.username,
+	password: env.sequelize.authenticationData.password,
+	dialect: env.sequelize.databaseType,
+	database: env.sequelize.initial,
+	dialectOptions: {
+		encrypt: env.sequelize.encrypt
+	}
+});
 
-	let logger;
+const redis = new Redis({
+	host: env.redis.host,
+	password: env.redis.password,
+	port: env.redis.ssl ? 6380 : 6379,
+	tls: env.redis.ssl
+});
 
-	loggerProvider('Testing')
-		.then(obj => {
-			logger = obj;
+const port = 1908;
+
+let logger;
+let server;
+
+describe('server testing', function () {
+
+	before('creating the server', async function () {
+		logger = await loggerProvider('Testing');
+		logger.debug('Before HELLO!');
+
+		server = await eerie({
+			debug: env.debug,
+			secret: env.secret,
+			options: {
+				rolesCreateRoles: {
+					1: [0],
+					666: [1, 0]
+				},
+				loginAfterRegister: env.options.loginAfterRegister,
+				passwordMethod: env.options.passwordMethod
+			},
+			sequelize,
+			redis
 		});
+		server.listen(port, () => {
+			logger.debug('Test server is up on ' + port + '.');
+		});
+	});
 
-	it('should say hello', function () {
+	it('should say hello', function (done) {
 		logger.info('HELLO!');
+		done();
 	});
 });
