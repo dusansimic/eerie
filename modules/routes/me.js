3596e665f522ff9bc7c077269fca23f97fa43684
nexylmao <nenadvvvvvvv@gmail.com>
2019-04-09 21:07:45 +0200

    GET /me cleared to not show password

--- modules/routes/me.js
+++ modules/routes/me.js
@@ -1,7 +1,15 @@
 module.exports = () => {
 	return async (req, res, _) => {
 		return res.status(200).send({
-			account: req.account
+			id: req.account.id,
+			account: {
+				username: req.account.username,
+				email: req.account.email,
+				role: req.account.role,
+				status: req.account.status,
+				createdAt: req.account.createdAt,
+				updatedAt: req.account.updatedAt
+			}
 		});
 	};
 };
